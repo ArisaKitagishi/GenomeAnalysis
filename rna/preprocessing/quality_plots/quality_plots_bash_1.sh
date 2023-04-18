7b6d872957797e4658fd307a529587a3bbb4b0cc
Arisa Sta Rita Kitagishi <arki7242@rackham2.uppmax.uu.se>
2023-04-19 00:03:03 +0200

    updated Wed Apr 19 00:03:03 CEST 2023

--- rna/preprocessing/quality_plots/quality_plots_bash_1.sh
+++ rna/preprocessing/quality_plots/quality_plots_bash_1.sh
@@ -11,21 +11,23 @@ cd $SNIC_TMP
 # load modules
 module load bioinfo-tools
 module load FastQC
-
+echo "loaded modules"
 # paths
 rna_trimmed=$HOME/genomeAnalysis/rna/preprocessing/ERR*
+echo $rna_trimmed
 
 for x in $rna_trimmed
 do
-	file=$(basename)
+	file=$(basename $x)
 	echo "File is $file"
 
-	file_path="$SCRDIR/$file"
+	file_path="${SCRDIR}/${file}"
 	mkdir $file_path
-	echo "file path is $file_path"
+	echo "file path is ${file_path}"
 	for y in "${x}/*"
+	do
 		fastqc --outdir $file_path $y
-		echo "FastQC done for $y"
+		echo "FastQC done for ${y}"
 	done
 done
 
--- update.sh
+++ update.sh
@@ -1,7 +1,18 @@
-$HOME/genomeAnalysis/ -name '*bash*.sh' | cpio -pdm dna/
-git add dna 
-git add rna 
-git add ./*.sh
+# Extra lnes of code for reference 
+# /home/arki7242/genomeAnalysis/dna/assembly/assembly_bash_1.sh
+# find $HOME/genomeAnalysis/dna/ -name '*bash*.sh' -print | cpio -pdm dna/ 
+# find $HOME/genomeAnalysis/rna/ -name '*bash*.sh' -print | cpio -pdm rna/
+
+getridof=/home/arki7242/genomeAnalysis/
+for x in $(find $HOME/genomeAnalysis/ -name '*bash*.sh');
+do
+	# need to cut to avoid home 
+	srcdir="${x#$getridof}"
+	echo $srcdir	
+	cp -r $x ./${srcdir}
+done
+ 
+git add ./
 git commit -m "updated $(date)"
 git push 
 
